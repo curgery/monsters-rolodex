0a795cb71590dd5376f7832dfe0641c99a68b5b2
curgery <androidgeek54@gmail.com>
2019-07-08 12:00:37 -0400

    Updates

---
+++ precache-manifest.2e7b5e0ea57299b858eaa43be72356c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6b025747210c44680d8c7e4e09fdfe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5099653070ec67ea607c",
+    "url": "/monsters-rolodex/static/css/main.007e3d20.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "5099653070ec67ea607c",
+    "url": "/monsters-rolodex/static/js/main.679b2c7c.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
